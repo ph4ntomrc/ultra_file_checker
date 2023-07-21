2313034b8d52ca9a184cca2b5ef8d4c51f70cee5
ph4ntom <91190804+ph4ntomrc@users.noreply.github.com>
2023-07-22 00:43:18 +0300

    prikol

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from subprocess import check_output
+from flask import Flask, flash, request, redirect, url_for, render_template, render_template_string, send_from_directory
+from werkzeug.utils import secure_filename
+from flag import FLAG
+
+UPLOAD_FOLDER = '/mnt/d/mytasks/ultra_file_checker/uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            res = os.popen(f"file uploads/{filename}").read()
+            return render_template('index.html', output=res)
+        else:
+            return render_template_string("Прикол")
+    else:
+        return render_template('index.html')
+app.run("0.0.0.0", 8080)
